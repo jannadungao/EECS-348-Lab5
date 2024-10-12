35ef4981663e6091ae2757d0b5a1f98aefc73dab
jannadungao <jannadungao2@gmail.com>
2024-10-12 15:03:29 -0500

    edits

--- problem2/problem2.c
+++ problem2/problem2.c
@@ -3,25 +3,22 @@
 #define SIZE 5
 
 // add matrices
-int addMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
+int addMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int mnew[SIZE][SIZE]) {
     // check if you can add
     if (sizeof(m1[SIZE][SIZE]) != sizeof(m2[SIZE][SIZE])) {
         printf("Incompatible matrix dimensions for addition");
         return 0; 
     }
     // do addition
-    int mnew[SIZE][SIZE];
-
-    for (int row = 0; row < sizeof(m1); row++) {
-        for (int col = 0; col < sizeof(m1[0]); col++) {
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
             mnew[row][col] = m1[row][col] + m2[row][col];
         }
     }
-    return mnew[SIZE][SIZE];
 }
 
 // multiply matrices
-int multiplyMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
+int multiplyMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int mnew[SIZE][SIZE]) {
     // check if multiplication is possible (cols of m1 == rows of m2)
     if (sizeof(m1[0]) != sizeof(m1)) {
         printf("Incompatible matrix dimensions for multiplication");
@@ -29,35 +26,32 @@ int multiplyMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE]) {
     }
 
     // do multiplication
-    int mnew[sizeof(m1)][sizeof(m2[0])];
-
-    for (int row = 0; row < sizeof(m1); row++) {
-        int val = 0;
-        for (int col = 0; col < sizeof(m2[0]); col++) {
-            val = val + m1[row][col] + m2[col][0];
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            mnew = 0;
+            for (int i = 0; i < SIZE; i++) {
+                mnew += m1[row][i] * m2[i][col];
+            }
         }
     }
-    return mnew[sizeof(m1)][sizeof(m2[0])];
 }
 
 
 // transpose matrices
-int transposeMatrix(int m[SIZE][SIZE]) {
+int transposeMatrix(int m[SIZE][SIZE], int mnew[SIZE][SIZE]) {
     int mnew[SIZE][SIZE];
 
-    for (int col = 0; col < sizeof(m[0]); col++) {
+    for (int col = 0; col < SIZE; col++) {
         for (int row = 0; row < sizeof(m); row++) {
             mnew[col][row] = m[row][col];
         }
     }
-
-    return mnew[SIZE][SIZE];
 }
 
 // print matrix
-void printMatrix(int m) {
-    for (int row = 0; row < sizeof(m); row++) {
-        printf("%d\n", m[row]);
+void printMatrix(int m[SIZE][SIZE]) {
+    for (int row = 0; row < SIZE; row++) {
+        printf("%d\n", *m[row]);
     }
 }
 
@@ -77,17 +71,26 @@ int main() {
         {10, 9, 8, 7, 6},
         {5, 4, 3, 2, 1}
     };
-
     // function calls
     printf("Addition of Matrices: \n");
-    printMatrix(addMatrices(m1, m2));
+    int m3[SIZE][SIZE];
+    addMatrices(m1, m2, m3);
+    printMatrix(m3[SIZE][SIZE]);
 
     printf("Multiplication of Matrices: \n");
-    printMatrix(multiplyMatrices(m1, m2));
+    int m4[SIZE][SIZE];
+    multiplyMatrices(m1, m2, m4);
+    printMatrix(m4[SIZE][SIZE]);
 
     printf("Tranpsposition of first matrix: \n");
-    printMatrix(transposeMatrix(m1));
+    int m5[SIZE][SIZE];
+    transposeMatrix(m1, m5);
+    printMatrix(m5[SIZE][SIZE]);
 
     printf("Transposition of second matrix: \n");
-    printMatrix(transposeMatrix(m2));    
+    int m6[SIZE][SIZE];
+    transposeMatrix(m2, m6);
+    printMatrix(m6[SIZE][SIZE]);    
+    
+    return 0;
 }
